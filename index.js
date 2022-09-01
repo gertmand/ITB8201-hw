e25f4136e8969a2332e033aa6d52d1f9bd16f9de
Gert Mänd <gertmand1@gmail.com>
2022-09-01 17:31:57 +0300

    netlify

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const serverless = require('serverless-http')
 
 const app = express();
+const router = express.Router()
 
-app.get('/', (req, res) => {
+router.get('/',(req,res) => {
   if(req.query.nimi != undefined) {
     res.send('Tere, ' + req.query.nimi + "!");
   } else {
     res.send('Tere, John Doe!')
   }
-});
+})
 
-app.listen(3000, () => console.log('Example app is listening on port 3000!'));
+router.get('/test',(req,res) => {
+  res.json({
+      hello: "test!"
+    });
+
+})
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
+//app.listen(3000, () => console.log('Example app is listening on port 3000!'));
